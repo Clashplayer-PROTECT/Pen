2c16249362eea2398bea91be4cda7f7234851dfd
Clashplayer <69047238+Clashplayer-PROTECT@users.noreply.github.com>
2022-05-15 13:13:13 +0200

    Create index.sh

---
+++ index.sh
@@ -0,0 +1,5 @@
+curl -sSL https://get.docker.com/ | CHANNEL=stable bash
+systemctl enable --now docker
+sudo curl -L https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
+sudo chmod +x /usr/local/bin/docker-compose
+docker-compose --version
