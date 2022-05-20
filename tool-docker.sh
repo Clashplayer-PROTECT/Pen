0585795b1fde8aef45386e7065f4d7250706f907
Clashplayer <69047238+Clashplayer-PROTECT@users.noreply.github.com>
2022-05-20 09:07:26 +0200

    Update and rename index.sh to tool-docker.sh

--- tool-docker.sh
+++ tool-docker.sh
@@ -1,3 +1,4 @@
+#!/bin/bash
 curl -sSL https://get.docker.com/ | CHANNEL=stable bash
 systemctl enable --now docker
 sudo curl -L https://github.com/docker/compose/releases/download/v2.5.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
