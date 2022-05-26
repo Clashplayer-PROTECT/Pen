214bff8a6ea9b51c32b556ce79232c497f64b350
Clashplayer <69047238+Clashplayer-PROTECT@users.noreply.github.com>
2022-05-26 17:19:37 +0200

    Delete crowdsec.sh

--- crowdsec.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | sudo bash
-apt-get install crowdsec
-sudo apt install crowdsec-firewall-bouncer-iptables
-cscli dashboard setup --listen YOURIP
