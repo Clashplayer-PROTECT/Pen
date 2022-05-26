f9df7a636a6ab5892f5f44030bc4d8729d8ee5e0
Clashplayer <69047238+Clashplayer-PROTECT@users.noreply.github.com>
2022-05-26 16:52:21 +0200

    Update crowdsec.sh

--- crowdsec.sh
+++ crowdsec.sh
@@ -1,1 +1,5 @@
-
+#!/bin/bash
+curl -s https://packagecloud.io/install/repositories/crowdsec/crowdsec/script.deb.sh | sudo bash
+apt-get install crowdsec
+sudo apt install crowdsec-firewall-bouncer-iptables
+cscli dashboard setup --listen 194.9.172.55
